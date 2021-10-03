be3c33e1ce90dcf8cda9d7d15e8806eac79b97c2
Yuri Garcia Ribeiro <81641949+YuriGarciaRibeiro@users.noreply.github.com>
2021-10-03 10:10:45 -0300

    Update funcoes.py

--- questão 1 prova 1 unidade/funcoes.py
+++ questão 1 prova 1 unidade/funcoes.py
@@ -1,3 +1,10 @@
 def cadastrar_clientes(nome,cpf,arquivo):
-    dados = open(f'{arquivo}.txt','a')
-    for linha in dados:
+    arquivo_dados = open(f'{arquivo}.txt','a')
+    cont = 0
+    for linha in arquivo_dados:
+        dados = linha.split()
+        if dados[2] == cpf:
+            cont += 1
+    
+
+
